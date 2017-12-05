5f55205a10bfa2b57a8e7d9e5678303d732152ff
jecrell <matt.walls31@gmail.com>
2017-12-06 00:48:35 +0900

    Initial cleanup

---
+++ Source/WizardryDefOf.cs
@@ -0,0 +1,17 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Wizardry
+{
+    [DefOf]
+    public class WizardryDefOf
+    {
+        public static TraitDef Istari;
+        public static TraitDef MagicAttuned;
+    }
+
+}
